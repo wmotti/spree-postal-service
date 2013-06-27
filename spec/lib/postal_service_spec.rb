c742de9fbc85e5ad0f1d22c865426a3be47543ee
Tobias Bohwalli <hi@futhr.io>
2013-06-27 15:19:06 +0200

    Prep specs..

--- spec/lib/postal_service_spec.rb
+++ spec/lib/postal_service_spec.rb
@@ -1,5 +1,15 @@
 require 'spec_helper'
 
-describe "Spree::Calculator::PostalService" do
+describe Spree::Calculator::PostalService do
+  context "description" do
+  end
 
+  context "item_oversized?" do
+  end
+
+  context "available?" do
+  end
+
+  context "compute" do
+  end
 end
