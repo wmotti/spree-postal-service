856f8e4ac835c9a4f4afa9112425f6e7b9435a4e
Tobias Bohwalli <hi@futhr.io>
2013-07-26 13:46:45 +0200

    Fix errors from bad git merge.

--- spec/lib/postal_service_spec.rb
+++ spec/lib/postal_service_spec.rb
@@ -3,22 +3,6 @@ require 'spec_helper'
 describe Spree::Calculator::Shipping::PostalService do
 	let(:postal_service_calculator) { Spree::Calculator::Shipping::PostalService.new }
 
-<<<<<<< HEAD
-	context ":using the default weight-price table: [1 2 5 10 20] => [6 9 12 15 18]" do
-		describe "#compute:" do
-			it "gives 15.0 when total price is 100 and weight is 10kg" do
-				create_our_package(weight: 10.0, price: 100.0, quantity: 1)
-				result = postal_service_calculator.compute(@package)
-				result.should eq(15.0)
-			end
-
-			it "gives 25.0 when total price is 40 and weight is 10kg" do
-				create_our_package(weight: 10.0, price: 40.0, quantity: 1)
-				result = postal_service_calculator.compute(@package)
-				result.should eq(25.0)
-			end
-
-=======
   describe "return description" do
     context "in english" do
       before { I18n.locale = :en }
@@ -49,7 +33,6 @@ describe Spree::Calculator::Shipping::PostalService do
 				result.should eq(25.0)
 			end
 
->>>>>>> 2-0-stable
 			it "gives 6 when total price is 60 and weight is less than 1kg" do
 				create_our_package(weight: 0.5, price: 60.0, quantity: 1)
 				result = postal_service_calculator.compute(@package)
@@ -87,10 +70,6 @@ describe Spree::Calculator::Shipping::PostalService do
 			end
 
 			it "gives 0 when total price is more than the MAX, for any number of items" do
-<<<<<<< HEAD
-        pending "expected: 0.0 got: 30.0"
-=======
->>>>>>> 2-0-stable
 				create_our_package(weight: 25.0, price: 350.0, quantity: 1)
 				postal_service_calculator.preferred_max_price = 300
 				result = postal_service_calculator.compute(@package)
@@ -99,13 +78,8 @@ describe Spree::Calculator::Shipping::PostalService do
 		end
 	end
 
-<<<<<<< HEAD
-	context "when preferred max weight, length and width are 18 kg, 120 cm and 60 cm" do
-		describe "#available?" do
-=======
 	describe "when preferred max weight, length and width are 18 kg, 120 cm and 60 cm" do
 		context "#available?" do
->>>>>>> 2-0-stable
 			it "is false when item weighs more than 18kg" do
 			  create_our_package(weight: 20, height: 70, width: 30, depth: 30)
 			  postal_service_calculator.available?(@package.contents).should be_false
@@ -122,11 +96,7 @@ describe Spree::Calculator::Shipping::PostalService do
 			end
 		end
 
-<<<<<<< HEAD
-		describe "#item_oversized?" do
-=======
 		context "#item_oversized?" do
->>>>>>> 2-0-stable
 			it "is true if the longest side is more than 120cm" do
 			  create_our_package(weight: 10, height: 130, width: 40, depth: 30)
 			  postal_service_calculator.item_oversized?(@variant).should be_true
@@ -147,11 +117,7 @@ describe Spree::Calculator::Shipping::PostalService do
 		params.merge!(depth:  args[:depth])  if args[:depth]
 		@variant = create(:base_variant, params)
 
-<<<<<<< HEAD
-		params = {variant: @variant}
-=======
 		params = { variant: @variant }
->>>>>>> 2-0-stable
 		params.merge!(price: args[:price])       if args[:price]
 		params.merge!(quantity: args[:quantity]) if args[:quantity]
 		@line_item = create(:line_item, params)
